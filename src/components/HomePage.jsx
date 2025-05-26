27ca8f27021736af8820b041680e72d69682d870
li-maksim <max.mail4@yandex.ru>
2025-05-26 21:08:34 +0300

    Changed button on HomePage
    Also added small hover animations for links and buttons

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,19 @@
 import { Link } from 'react-router-dom'
 import styles from '../styles/HomePage.module.css'
 import imgLaptop from '../assets/laptop.jpg'
+import { useNavigate } from 'react-router-dom'
 
 function HomePage() {
+
+    const navigate = useNavigate()
 
     return (
         <div className={styles.homepage}>
             <div>
                 <h1 className={`h1, ${styles.h1}`}>Home Page</h1>
                 <p className={styles.p}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo, aliquam sint! Voluptates facere provident saepe ratione tempora vel quae! Odit veritatis quas totam doloremque quisquam numquam at nulla nostrum molestias.</p>
-                <button className={`btn, ${styles.btn}`}>
-                    <Link to="/store" className={`link`}>Start Shoping</Link>
+                <button className={`btn, ${styles.btn}`} onClick={() => navigate('/store')}>
+                    Start Shopping
                 </button>
             </div>
             <div>
